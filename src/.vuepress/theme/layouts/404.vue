fa5894be053d326b5b02808715a59afeb3a2c60a
Jimmy McCann <jabez.m.007@gmail.com>
2020-12-11 21:06:25 -0600

    simplify 404 layout

--- src/.vuepress/theme/layouts/404.vue
+++ src/.vuepress/theme/layouts/404.vue
@@ -1,20 +1,14 @@
 <template>
   <div class="h-full flex items-center">
     <div class="flex-grow flex flex-col">
-      <div>
-        <h1 class="text-center text-5xl font-bold">Oops</h1>
-      </div>
-      <div class="flex justify-center">
-        <img src="../assets/notFound.png" />
-      </div>
-      <div>
-        <p class="text-center text-3xl font-semibold">
-          It looks like this site needs a bit more time in the oven
-        </p>
-        <!--p class="text-center text-3xl font-semibold">
-          It looks like this page needs a bit more time in the oven
-        </p-->
-    </div>
+      <h1 class="text-center text-5xl font-bold">Oops</h1>
+      <img src="../assets/notFound.png" />
+      <p class="text-center text-3xl font-semibold">
+        It looks like this site needs a bit more time in the oven
+      </p>
+      <!--p class="text-center text-3xl font-semibold">
+        It looks like this page needs a bit more time in the oven
+      </p-->
     </div>
   </div>
 </template>
