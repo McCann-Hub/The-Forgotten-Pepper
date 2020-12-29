19f1b6165fb2ef738207e43e3880a78baeccb31b
Jimmy McCann <jabez.m.007@gmail.com>
2020-12-28 21:23:33 -0600

    cleaner UI

--- src/.vuepress/theme/global-components/IngredientOverlayout.vue
+++ src/.vuepress/theme/global-components/IngredientOverlayout.vue
@@ -1,24 +1,16 @@
 <template>
-  <div id="overlay" class="flex flex-col">
-    <div class="bg-white bg-opacity-50 flex justify-end content-center pr-4">
-      <a @click="$emit('close')">
-        <!-- Heroicon name: x -->
+  <div id="overlay">
+    <a @click="$emit('close')" class="absolute right-1 top-0 cursor-pointer">
         <svg
-          class="w-5 h-5"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 20 20"
+          class="fill-current text-gray-500 hover:text-gray-50 w-7 h-7"
           xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 18 18"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M6 18L18 6M6 6l12 12"
-          ></path>
+            d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"
+          />
         </svg>
       </a>
-    </div>
     <ingredient :pages="pages" />
   </div>
 </template>
@@ -48,7 +40,7 @@ export default {
   left: 0;
   right: 0;
   bottom: var(--footer-height);
-  background-color: rgba(0, 0, 0, 0.5); /* Black background with opacity */
+  background-color: rgba(0, 0, 0, 0.75); /* Black background with opacity */
   z-index: 2; /* Specify a stack order in case you're using a different order for other elements */
 }
 </style>
