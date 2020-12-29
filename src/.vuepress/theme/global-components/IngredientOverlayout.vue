7713b27bfdddf5e1e289080237607d082666be49
Jimmy McCann <jabez.m.007@gmail.com>
2020-12-28 21:24:24 -0600

    format

--- src/.vuepress/theme/global-components/IngredientOverlayout.vue
+++ src/.vuepress/theme/global-components/IngredientOverlayout.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="overlay">
     <a @click="$emit('close')" class="absolute right-1 top-0 cursor-pointer">
-        <svg
-          class="fill-current text-gray-500 hover:text-gray-50 w-7 h-7"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 18 18"
-        >
-          <path
-            d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"
-          />
-        </svg>
-      </a>
+      <svg
+        class="fill-current text-gray-500 hover:text-gray-50 w-7 h-7"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 18 18"
+      >
+        <path
+          d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"
+        />
+      </svg>
+    </a>
     <ingredient :pages="pages" />
   </div>
 </template>
