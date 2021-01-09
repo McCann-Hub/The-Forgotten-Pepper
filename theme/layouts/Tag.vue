2944b754057ed682d34a27da729ea672dce3fdb5
Jimmy McCann <jabez.m.007@gmail.com>
2021-01-08 20:45:16 -0600

    fallback to FrontmatterPagination

--- theme/layouts/Tag.vue
+++ theme/layouts/Tag.vue
@@ -1,22 +1,9 @@
 <template>
-  <div
-    class="relative min-h-full min-w-full flex flex-col justify-center items-center"
-  >
-    <div
-      class="min-h-full min-w-full flex flex-wrap justify-evenly items-center"
-    >
-      <page-card v-for="page in _pages" :key="page.key" :page="page" />
-    </div>
-    <div v-if="pages.length < 1" class="absolute bottom-0 w-full">
-      <div class="flex justify-center items-center">
-        <Pagination id="pagination" />
-      </div>
-    </div>
-  </div>
+  <frontmatter-pagination :pages="_pages" />
 </template>
 
 <script>
-import { Pagination } from "@vuepress/plugin-blog/lib/client/components";
+import FrontmatterPagination from "./FrontmatterPagination.vue";
 
 export default {
   name: "Tag",
@@ -27,7 +14,7 @@ export default {
     },
   },
   components: {
-    Pagination,
+    FrontmatterPagination,
   },
   computed: {
     _pages() {
