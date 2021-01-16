6a7d2106f816337925d1151d230a2e78a656de1f
Jimmy McCann <jabez.m.007@gmail.com>
2021-01-16 00:21:46 -0600

    create default FrontmatterKey layout

--- theme/global-components/TagPill.vue
+++ theme/global-components/TagPill.vue
@@ -3,7 +3,7 @@
     class="rounded-full px-3 py-1 text-sm font-semibold cursor-pointer bg-gray-700 text-gray-200 dark:bg-gray-200 dark:text-gray-700"
     @click="$router.push(tag.path)"
   >
-    {{ `#${tag.tag}` }}
+    {{ `#${tag.tag || tag.name}` }}
   </div>
 </template>
 
---
+++ theme/layouts/FrontmatterKey.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-wrap justify-evenly items-center">
+    <div v-for="tag in $frontmatterKey.list" :key="tag.name">
+      <tag-pill :tag="tag" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FrontmatterKey",
+};
+</script>
