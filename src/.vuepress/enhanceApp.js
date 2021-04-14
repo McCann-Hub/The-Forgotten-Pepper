1b93e75d6d81a5c526f983f362d056f7adf12995
Jimmy McCann <jabez.m.007@gmail.com>
2021-04-14 17:51:09 -0500

    adsense tag

--- src/.vuepress/enhanceApp.js
+++ src/.vuepress/enhanceApp.js
@@ -5,5 +5,12 @@ export default ({
   siteData, // site metadata
   isServer, // is this enhancement applied in server-rendering or client
 }) => {
-  // ...apply enhancements to the app
+  if (process.env.NODE_ENV === 'production' && typeof window !== 'undefined') {
+    const adsense = document.createElement('script');
+    adsense.setAttribute('data-ad-client', 'ca-pub-3040760297930263');
+    adsense.async = true;
+    adsense.src =
+      'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
+    document.head.appendChild(adsense);
+  }
 };
